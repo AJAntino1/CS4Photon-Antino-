246b430be77509880d69a4cde5efa6ccfc033aba
AJ Antino <93103876+AJAntino1@users.noreply.github.com>
2022-11-17 07:42:21 +0800

    Add files via upload

---
+++ Q2Ex6/WaterType.java
@@ -0,0 +1,9 @@
+package exercise06photonantinoaj;
+
+public class WaterType extends Monster{
+    
+    public WaterType(String name, int hp, int base){
+        super("w", "water", "fire", "grass", 100, 10);
+    }
+}
+
