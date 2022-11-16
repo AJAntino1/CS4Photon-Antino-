ae72f0aa539da0a13da4d68e59c9e00ead1303fe
AJ Antino <93103876+AJAntino1@users.noreply.github.com>
2022-11-17 07:40:50 +0800

    Delete GrassType.java

--- GrassType.java
+++
@@ -1,9 +0,0 @@
-package exercise06photonantinoaj;
-
-public class GrassType extends Monster{
-    
-    public GrassType(String name, int hp, int base){
-        super("g", "grass", "water", "fire", 100, 10);
-    }
-}
-
