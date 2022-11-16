e2fac1ea00e49d2fe19fc8a1dd5b687bfc6b52ed
AJ Antino <93103876+AJAntino1@users.noreply.github.com>
2022-11-17 07:43:30 +0800

    Add files via upload

---
+++ Q2Ex6/GrassType.java
@@ -0,0 +1,9 @@
+package exercise06photonantinoaj;
+
+public class GrassType extends Monster{
+    
+    public GrassType(String name, int hp, int base){
+        super("g", "grass", "water", "fire", 100, 10);
+    }
+}
+
