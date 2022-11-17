e12785979fd92faa54fb7510c7855effe5907830
AJ Antino <93103876+AJAntino1@users.noreply.github.com>
2022-11-17 08:57:35 +0800

    Update FireType.java

--- Q2Ex6/FireType.java
+++ Q2Ex6/FireType.java
@@ -3,14 +3,17 @@ package exercise06photonantinoaj;
 public class FireType extends Monster{
     
     public FireType(String name, int hp, int base){
-        super("f", "fire", "grass", "water", 100, 10);
-        
+        super("f", "fire", "grass", "water", hp, base);
+        this.atk = (int) (base*1.3);
+        this.def = (int) (base*0.7);
     }
     
     @Override
     public void special(){
         super.special();
-        getAtk();
-        
+        this.atk = atk + 2;
+        this.hp = (int) (hp - maxHP*0.1);
+        System.out.println(name + "increased their atk by 2 points. Their atk is now " + atk);
+        System.out.println(name + "'s HP is now " + hp);
     }
 }
