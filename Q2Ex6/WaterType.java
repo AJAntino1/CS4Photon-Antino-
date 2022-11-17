ae87417ddc10c75f7e41aa8bfa086622d332aa0e
AJ Antino <93103876+AJAntino1@users.noreply.github.com>
2022-11-17 08:58:36 +0800

    Update WaterType.java

--- Q2Ex6/WaterType.java
+++ Q2Ex6/WaterType.java
@@ -3,7 +3,19 @@ package exercise06photonantinoaj;
 public class WaterType extends Monster{
     
     public WaterType(String name, int hp, int base){
-        super("w", "water", "fire", "grass", 100, 10);
+        super("w", "water", "fire", "grass", hp, base);
+        this.atk = (int) (base*0.7);
+        this.def = (int) (base*1.3);
+    }
+    
+    @Override
+    public void special(){
+        super.special();
+        this.def = def + 2;
+        this.hp = (int) (hp - maxHP*0.1);
+        System.out.println(name + "'s Def is now " + def);
+        System.out.println(name + "'s HP is now " + hp);
     }
 }
+
 
