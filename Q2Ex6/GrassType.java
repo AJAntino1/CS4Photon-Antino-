fe42668a20dc234e6ed29bb87e02820d1bd16ed1
AJ Antino <93103876+AJAntino1@users.noreply.github.com>
2022-11-17 08:58:01 +0800

    Update GrassType.java

--- Q2Ex6/GrassType.java
+++ Q2Ex6/GrassType.java
@@ -3,7 +3,21 @@ package exercise06photonantinoaj;
 public class GrassType extends Monster{
     
     public GrassType(String name, int hp, int base){
-        super("g", "grass", "water", "fire", 100, 10);
+        super("g", "grass", "water", "fire", hp, base);
+        this.atk = base;
+        this.def = base;
+    }
+    
+    @Override
+    public void special(){
+        super.special();
+        this.hp = (int) (hp + maxHP*0.2);
+        System.out.println(name + hp);
+    }
+    
+    public void rest(){
+        this.hp = (int) (hp + maxHP*0.5);
+        System.out.println(name + "'s HP is now " + hp);
     }
 }
 
